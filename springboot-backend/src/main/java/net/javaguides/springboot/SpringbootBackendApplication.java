de40aa5888a25a531aed04ce062f51797500d05e
root <root@ip-172-31-82-152.ec2.internal>
2025-06-20 19:00:08 +0000

    edited all files

--- springboot-backend/src/main/java/net/javaguides/springboot/SpringbootBackendApplication.java
+++ springboot-backend/src/main/java/net/javaguides/springboot/SpringbootBackendApplication.java
@@ -2,12 +2,15 @@ package net.javaguides.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "net.javaguides.springboot")
+@EnableJpaRepositories(basePackages = "net.javaguides.springboot.repository")
 public class SpringbootBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootBackendApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootBackendApplication.class, args);
+    }
 }
