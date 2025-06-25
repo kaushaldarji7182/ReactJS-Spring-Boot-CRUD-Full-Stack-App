b529ad45e5766cfadce12ef551f0fbeb87ad39be
root <root@ip-172-31-94-142.ec2.internal>
2025-06-25 07:00:50 +0000

    Your commit message

--- react-frontend/src/services/EmployeeService.js
+++ react-frontend/src/services/EmployeeService.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const EMPLOYEE_API_BASE_URL = `${process.env.REACT_APP_BACKEND_URL}/api/v1/employees`;
+//const EMPLOYEE_API_BASE_URL = `${process.env.REACT_APP_BACKEND_URL}/api/v1/employees`;
+const EMPLOYEE_API_BASE_URL = `${process.env.REACT_APP_BACKEND_URL}/employees`;
 
 class EmployeeService {
     getEmployees() {
